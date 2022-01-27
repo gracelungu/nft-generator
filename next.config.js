13fe3d65a2898330c8137b4480fc95bbd66f8d66
gracelbw <grace.lungu.bw@gmail.com>
2022-01-27 19:18:54 +0200

    config solution

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
 const withSvgr = require("next-svgr");
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 module.exports = {
   typescript: {
     ignoreBuildErrors: true,
