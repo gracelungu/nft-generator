ce32b2bd5c6bfed784a39a4841f23f4a4a3ca01a
gracelbw <grace.lungu.bw@gmail.com>
2022-01-27 19:53:10 +0200

    Fix style

--- src/components/common/BottomNav/index.tsx
+++ src/components/common/BottomNav/index.tsx
@@ -4,7 +4,7 @@ import styles from "./index.module.scss";
 function BottomNav() {
   return (
     <div className={styles.footer}>
-      <p>COPYRIGHT © art-art-NFT {new Date().getFullYear()}</p>
+      <p>COPYRIGHT © NFTooze {new Date().getFullYear()}</p>
     </div>
   );
 }
